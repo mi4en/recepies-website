f1ec1cd7c729f577666fe463ba1bcfd85c1733db
Mario <marionikolov87@gmail.com>
2018-09-18 13:34:02 +0000

    add mongo connection mLab

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var commentRoutes    = require('./routes/comments'),
     campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes      = require('./routes/index')
     
-mongoose.connect('mongodb://localhost/yelp_camp', {useNewUrlParser: true})
+// mongoose.connect('mongodb://localhost/yelp_camp', {useNewUrlParser: true})
+mongoose.connect('mongodb://admin:admin1@ds159812.mlab.com:59812/campingsites', {useNewUrlParser: true})
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
