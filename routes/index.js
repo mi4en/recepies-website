2dce088be63314b0244520a0b2a2e507e63fabbd
Mario <marionikolov87@gmail.com>
2018-09-27 18:41:28 +0000

    UI improvements, nav bar improvements, site update to be responsive on mobile

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function (req, res) {
 
 // show register form
 router.get('/register', function(req, res) {
-    res.render('register')
+    res.render('register', {page: 'register'})
 })
 
 // sign up logic
@@ -31,9 +31,9 @@ router.post('/register', function(req, res) {
 })
 
 
-// render login form
+// show login form
 router.get('/login', function(req, res) {
-    res.render('login')
+    res.render('login', {page: 'login'})
 })
 
 // login logic
