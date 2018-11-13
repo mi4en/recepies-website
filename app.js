71131c2ef4c90bc0959c6f9fa3fb2d25a7dd4fae
Mario Nikolov <m.nikolov@isystems.bg>
2018-11-13 10:15:42 +0200

    migrate project to vs code

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-var express           = require('express'),
-    app               = express(),
-    bodyParser        = require('body-parser'),
-    mongoose          = require('mongoose'),
-    flash             = require('connect-flash'),
-    moment            = require('moment'),
-    passport          = require('passport'),
-    LocalStrategy     = require('passport-local'),
-    methodOverride    = require('method-override'),
-    Campground        = require('./models/campground'),
-    Comment           = require('./models/comment'),
-    User              = require('./models/user'),
-    seedDB            = require('./seeds')
-    
-//requiring routes
-var commentRoutes    = require('./routes/comments'),
-    campgroundRoutes = require('./routes/campgrounds'),
-    indexRoutes      = require('./routes/index')
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var flash = require('connect-flash')
+var moment = require('moment')
+var passport = require('passport')
+var LocalStrategy = require('passport-local')
+var methodOverride = require('method-override')
+var Campground = require('./models/campground')
+var Comment = require('./models/comment')
+var User = require('./models/user')
+var seedDB = require('./seeds')
 
-//exprot DATABASEURL=mongodb://conn.string...   
+// requiring routes
+var commentRoutes = require('./routes/comments')
+
+var campgroundRoutes = require('./routes/campgrounds')
+
+var indexRoutes = require('./routes/index')
+
+// exprot DATABASEURL=mongodb://conn.string...
 var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp'
-mongoose.connect(url, {useNewUrlParser: true})
-app.use(bodyParser.urlencoded({extended: true}))
+mongoose.connect(
+  url,
+  { useNewUrlParser: true }
+)
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
 app.use(methodOverride('_method'))
@@ -31,33 +36,36 @@ app.use(flash())
 app.locals.moment = require('moment')
 
 // PASSPORT CONFIGURATION
-app.use(require('express-session')({
+app.use(
+  require('express-session')({
     secret: 'Demon hunter is the best class!',
     resave: false,
     saveUninitialized: false
-}));
+  })
+)
 app.use(passport.initialize())
 app.use(passport.session())
 passport.use(new LocalStrategy(User.authenticate()))
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user
-   res.locals.error = req.flash('error')
-   res.locals.success = req.flash('success')
-   next()
-});
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user
+  res.locals.error = req.flash('error')
+  res.locals.success = req.flash('success')
+  next()
+})
 
 app.use('/', indexRoutes)
 app.use('/campgrounds', campgroundRoutes)
 app.use('/campgrounds/:id/comments', commentRoutes)
 
-app.use(function(req, res, next){
-    res.status(404).render('404_error_template', {title: "Sorry, page not found"});
-});
+app.use(function (req, res, next) {
+  res
+    .status(404)
+    .render('404_error_template', { title: 'Sorry, page not found' })
+})
 
-
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log('server started...')
+app.listen(process.env.PORT || 3000, process.env.IP, function () {
+  console.log('server started...')
 })
